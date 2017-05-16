6b9314a52b5a16f807e45fcf53388d510e502145
chhoin <heistall9@gmail.com>
2017-05-16 21:01:14 +0700

    master page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+
+Route::get('/test', 'TestController@index');
+Route::get('/admin', 'DasboardController@index');
+
